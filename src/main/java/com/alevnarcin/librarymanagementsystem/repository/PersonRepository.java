d53dc239a52ea6295222293dd0bc1d4bb40901dc
Alev-Narcin <76040790+Alev-Narcin@users.noreply.github.com>
2021-01-11 17:26:51 +0300

    added findALl

--- src/main/java/com/alevnarcin/librarymanagementsystem/repository/PersonRepository.java
+++ src/main/java/com/alevnarcin/librarymanagementsystem/repository/PersonRepository.java
@@ -6,14 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sun.security.util.Password;
 
-import java.lang.annotation.Native;
-
 
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
 
-    @Query("from com.alevnarcin.librarymanagementsystem.entity.PersonEntity p where p.name is not null")
     String existByPasswordAndName(String password, String name);
 
-    PersonEntity findByNameNotNull();
 }
